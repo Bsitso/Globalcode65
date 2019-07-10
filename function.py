72d1ddf62b0a88e2a157bda2d375432c86ae9957
bridget <lohsitsofe60@gmail.com>
2019-07-10 14:38:39 +0000

    my first commit

---
+++ function.py
@@ -0,0 +1,40 @@
+'''def bridget ():
+    for kk in range (0, 4):
+     print('****')
+     
+bridget ()
+
+def we ():
+    for kk in range (0, 5):
+        print("*" * kk)
+    
+we ()
+
+bb = 3
+while (bb >= 0):
+    print("*" * bb)
+    bb = bb - 1'''
+
+
+    
+
+
+
+'''def kk (word):
+    print(word)
+    for letter in word:
+        print (letter)
+        if letter == "a":
+             return True
+    return False
+print(kk("cat"))
+print(kk("dog"))'''
+
+i = 2
+if(i % 2 == 0):
+    print ("Even Number")
+else:
+    print ("Odd Number")
+
+
+      
